d7886b966cb9e978d2e755eadb0df2e1f45f51a9
chizor iwuh <thisischizor@gmail.com>
2023-03-16 08:09:04 +0100

    refresh supabase on client

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -6,6 +6,24 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { Modal, storePopup } from '@skeletonlabs/skeleton';
 
+	import { invalidate } from '$app/navigation';
+  import { onMount } from 'svelte';
+  import type { LayoutData } from './$types';
+
+  export let data: LayoutData;
+
+  $: ({ supabase } = data);
+
+  onMount(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(() => {
+      invalidate('supabase:auth');
+    });
+
+    return () => subscription.unsubscribe();
+  });
+
 </script>
 
 <!-- <Solana /> -->
